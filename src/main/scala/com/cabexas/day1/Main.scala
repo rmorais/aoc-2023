c5978e82da896a115a683c07182a9bbc59a4bbb3
rmorais <2330276+rmorais@users.noreply.github.com>
2023-12-05 21:16:29 +0000

    Add day 1 part 2 incorrect solution

--- src/main/scala/com/cabexas/day1/Main.scala
+++ src/main/scala/com/cabexas/day1/Main.scala
@@ -3,31 +3,59 @@ package com.cabexas
 import scala.io.Source
 import scala.util.Using
 import scala.util.Try
+import scala.util.matching.Regex
+import scala.util.matching.Regex.Match
 
-val lines = readFile("src/main/resources/day1.txt").linesIterator
+def lines = readFile("src/main/resources/day1.txt").linesIterator
 
-val findCalibrationValues: List[Int] = {
-  val builder = List.newBuilder[Int]
-  var firstDigit = 'a'
-  var lastDigit = 'a'
-  for (line <- lines) {
-    for (char <- line) {
-      Try(Integer.parseInt(char.toString())).map { d =>
-        if firstDigit == 'a' then
-          firstDigit = char
-        lastDigit = char
-      }
-    }
-    builder.addOne((s"$firstDigit$lastDigit").toInt)
-    firstDigit = 'a'
-    lastDigit = 'a'
-  }
-
-  builder.result()
+def part1(input: Iterator[String]) = {
+  val regex = "[0-9]".r
+  input
+    .map: line =>
+      val matches = regex
+        .findAllMatchIn(line)
+        .map(_.matched.toInt)
+        .toList
+      val firstDigit = matches.head
+      val lastDigit = matches.last
+      firstDigit * 10 + lastDigit
+    .toList
 }
 
-@main def part1: Unit ={
-  println(s"Calibration values sum: [${findCalibrationValues.sum}]")
+val translation = Map[String, Int](
+  "one" -> 1,
+  "two" -> 2,
+  "three" -> 3,
+  "four" -> 4,
+  "five" -> 5,
+  "six" -> 6,
+  "seven" -> 7,
+  "eight" -> 8,
+  "nine" -> 9
+)
+
+def part2(input: Iterator[String]): List[Int] = {
+  val regex: Regex = "[0-9]|one|two|three|four|five|six|seven|eight|nine".r
+
+  input
+    .map: line =>
+      val matches = regex
+        .findAllMatchIn(line)
+        .map: m =>
+          translation
+            .get(m.matched)
+            .getOrElse(m.matched.toInt)
+        .toList
+      val firstDigit = matches.head
+      val lastDigit = matches.last
+      firstDigit * 10 + lastDigit
+    .toList
+
+}
+
+@main def main: Unit = {
+  println(s"Calibration values sum: [${part1(lines).sum}]")
+  println(s"Calibration values sum: [${part2(lines).sum}]")
 }
 
 //Using ensures file is closed
